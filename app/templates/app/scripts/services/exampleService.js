2a49687b2862bf75e8012213ba33e3b85bcc0456
homerjam <jameshomer85@gmail.com>
2013-11-28 18:16:54 +0000

    add example service

---
+++ app/templates/app/scripts/services/exampleService.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    angular.module('webApp.services')
+
+        .factory('exampleService', ['$http',
+            function exampleService($http) {
+
+                var privateVar = 'Hello World';
+
+                var service = {};
+
+                service.get = function () {
+                    return privateVar;
+                };
+
+                return service;
+            }
+        ]);
+
+})();
